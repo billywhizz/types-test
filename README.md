794d1751c75c2787d732f09c9eef2ae15a74f6d5
Andrew Johnston <apjohnsto@gmail.com>
2026-04-30 00:50:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+npm install @types/node@24
+mkdir ../types
+mv node_modules/@types/node ../types
+npm install @types/node@20
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as fs from "node:fs"
+import * as sqlite from "node:sqlite"
+
+const buf = fs.readFileSync("./index.js.map");
+
